27bf9e9a8b215e703770eee3a86cea9d9850ca90
afoninaaa <90454317+afoninaaa@users.noreply.github.com>
2024-06-24 18:10:14 +0300

    Update request.py

--- request.py
+++ request.py
@@ -10,12 +10,14 @@ def request_config():
 def request_com():
     comport_number = request.form['comportSelect']
     return comport_number
+    
 def request_command():
     device_addr = request.form.get('deviceAddr')
     command_no = request.form.get('commandNo')
     register = request.form.get('register')
     value = request.form.get('value')
     return device_addr, command_no, register, value
+    
 def request_table(index):
     comment1 = request.form.get(f'comment1_{index}')
     comment2 = request.form.get(f'comment2_{index}')
